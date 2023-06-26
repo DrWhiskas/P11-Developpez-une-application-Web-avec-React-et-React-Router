06a69df0df0d08373af864cf83d9bd1ff91c8620
unknown <kayv@hotmail.fr>
2023-06-26 12:52:23 +0200

    .

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ root.render(
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/About" element={<About />} />
+				<Route path="/localisation:Id" element={<LocalisationDetails />} />
 			</Routes>
 		</BrowserRouter>
 	</React.StrictMode>
@@ -23,6 +24,5 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-				//<Route path="/localisation:Id" element={<LocalisationDetails />} />
 
 reportWebVitals();
