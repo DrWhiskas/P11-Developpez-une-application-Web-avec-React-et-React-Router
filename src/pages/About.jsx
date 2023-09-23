312ecc2216e60fdf6ed5676406da6939913b8230
unknown <kayv@hotmail.fr>
2023-09-23 10:35:01 +0200

    .

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,18 +4,22 @@ import Banner from '../components/Banner';
 import Footer from '../components/Footer';
 import BannerBackgroundPropo from '../assets/Banner2.png';
 import data from '../data/dataAbout';
-import body from '../style/style.css'
+import '../style/style.css'
 import Dropdown from '../components/Dropdown';
 
 export default function About() {
   return (
-    <>
-      <Header />
-      <Banner img={BannerBackgroundPropo} text='' />
-      {data.map((item) => (
-        <Dropdown key={item.id} titles={item.title} descriptions={item.description} />
-      ))}
-      <Footer />
-    </>
-  );
+		<>
+			<Header />
+			<Banner img={BannerBackgroundPropo} text="" />
+			{data.map((item) => (
+				<Dropdown
+					key={item.id}
+					titles={item.title}
+					descriptions={item.description}
+				/>
+			))}
+			<Footer />
+		</>
+	);
 }
