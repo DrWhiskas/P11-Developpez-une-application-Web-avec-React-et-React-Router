162a0752f9d6c8eba9d8b3ca8243c786c3dfcac7
unknown <kayv@hotmail.fr>
2023-10-11 03:52:16 +0200

    active link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,39 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom';
 import logo from '../assets/logo.png'
 import '../style/header.css'
 
 export default function Header(){
+    const location = useLocation()
     return (
-    <div>
-        <nav className='header'>
-            <Link to="/">
-                <img src={logo} alt='Logo du site Kasa' className='header__logo' />
-            </Link>
-            <div className='header__nav'>
-                <Link to='/' className='header__nav__link'>
-                    Accueil
-                </Link>
-                <Link to='/about'className='header__nav__link'>
-                    A Propos
-                </Link>
-            </div>
-        </nav>
-    </div>
-    )
+			<div>
+				<nav className="header">
+					<Link to="/">
+						<img src={logo} alt="Logo du site Kasa" className="header__logo" />
+					</Link>
+					<div className="header__nav">
+						<Link
+							to="/"
+							className={
+								location.pathname === '/'
+									? 'active header__nav__link'
+									: 'header__nav__link'
+							}
+						>
+							Accueil
+						</Link>
+						<Link
+							to="/about"
+							className={
+								location.pathname === '/about'
+									? 'active header__nav__link'
+									: 'header__nav__link'
+							}
+						>
+							A Propos
+						</Link>
+					</div>
+				</nav>
+			</div>
+		);
 }
